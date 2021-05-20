9d9bdcd2c063fc594864c4e03b4fa8a5621cb405
Evgeny Shtanov <shtanov_evgenii@mail.ru>
2021-05-20 10:13:02 +0300

    Add --press option to save on 's' button press only. Fix --all Remove unused matplotlib.
    Signed-off-by: Evgeny Shtanov <shtanov_evgenii@mail.ru>

--- py/video_cutter.py
+++ py/video_cutter.py
@@ -4,12 +4,12 @@ import imutils
 import numpy as np
 import os
 import sys
-from matplotlib import pyplot as plt
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--video", required=True, help="Path to source video.")
 ap.add_argument("-o", "--out", required=False, help="Output folder path.")
-ap.add_argument("-a", "--all", required=False, help="Save all channnels: i,r,g,b,h,s,v.")
+ap.add_argument("-a", "--all", required=False, help="Save all channnels: i,r,g,b,h,s,v.", action='store_true')
 ap.add_argument("-w", "--rgb", required=False, help="Save cut of original image(RGB).", action='store_true')
 ap.add_argument("-i", "--gray", required=False, help="Save gray channnel(intensity).", action='store_true')
 ap.add_argument("-r", "--red", required=False, help="Save red channnel.", action='store_true')
@@ -24,6 +24,7 @@ ap.add_argument("-W", "--width", required=False, help="width of ROI.")
 ap.add_argument("-H", "--height", required=False, help="height of ROI.")
 ap.add_argument("-x", "--X", required=False, help="left x coord of ROI.")
 ap.add_argument("-y", "--Y", required=False, help="top y coord of ROI.")
+ap.add_argument("-p", "--press", required=False, help="Save on 's' button press.", action='store_true')
 args = vars(ap.parse_args())
 
 cap = cv2.VideoCapture(args["video"])
@@ -41,39 +42,38 @@ out_dir = "."
 if args["out"] is not None:
     out_dir = args["out"]
 
-if args["rgb"]:
+if args["rgb"] or args["all"]:
     os.makedirs(out_dir + "/rgb", mode=0o777, exist_ok=True)
 
-if args["red"]:
+if args["red"] or args["all"]:
     os.makedirs(out_dir + "/red", mode=0o777, exist_ok=True)
 
-if args["green"]:
+if args["green"] or args["all"]:
     os.makedirs(out_dir + "/green", mode=0o777, exist_ok=True)
 
-if args["blue"]:
+if args["blue"] or args["all"]:
     os.makedirs(out_dir + "/blue", mode=0o777, exist_ok=True)
 
-if args["hue"]:
+if args["hue"] or args["all"]:
     os.makedirs(out_dir + "/hue", mode=0o777, exist_ok=True)
 
-if args["sat"]:
+if args["sat"] or args["all"]:
     os.makedirs(out_dir + "/sat", mode=0o777, exist_ok=True)
 
-if args["val"]:
+if args["val"] or args["all"]:
     os.makedirs(out_dir + "/val", mode=0o777, exist_ok=True)
 
-if args["gray"]:
+if args["gray"] or args["all"]:
     os.makedirs(out_dir + "/gray", mode=0o777, exist_ok=True)
 
-# while(cap.isOpened()):
 i=0
-while(True):
+while(cap.isOpened()):
     i = i + 1
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     if args["rotate"] is not None:
-        #rotation angle in degree
+        # Rotation angle in degree
         #frame = ndimage.rotate(frame, -float(args["rotate"]))
         frame = imutils.rotate(frame, angle=-float(args["rotate"]))
 
@@ -81,40 +81,48 @@ while(True):
         frame = frame[y : y + h, x : x + w, :]
 
     # Our operations on the frame come here
-    if args["gray"]:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite(out_dir + '/gray/' + 'gray_' + "{}".format(i) + '.png', gray)
-
-    if args["sat"] or args["hue"] or args["val"]:
-        frag_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    if args["rgb"]:
-        cv2.imwrite(out_dir + '/rgb/' + "{}".format(i) + '.png', frame)
-
-    if args["red"]:
-        cv2.imwrite(out_dir + '/red/' + 'red_' + "{}".format(i) + '.png', frame[:, :, 0])
-
-    if args["green"]:
-        cv2.imwrite(out_dir + '/green/' + 'green_' + "{}".format(i) + '.png', frame[:, :, 1])
-
-    if args["blue"]:
-        cv2.imwrite(out_dir + '/blue/' + 'blue_' + "{}".format(i) + '.png', frame[:, :, 2])
-
-    if args["hue"]:
-        cv2.imwrite(out_dir + '/hue/' + 'hue_' + "{}".format(i) + '.png', frag_hsv[:, :, 0])
-
-    if args["sat"]:
-        cv2.imwrite(out_dir + '/sat/' + 'sat_' + "{}".format(i) + '.png', frag_hsv[:, :, 1])
-
-    if args["val"]:
-        cv2.imwrite(out_dir + '/val/' + 'val_' + "{}".format(i) + '.png', frag_hsv[:, :, 2])
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
 
     # Press Q on keyboard to exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
-        sys.exit("quit")
-        os._exit(0)
+        #os._exit(0)
         break
+
+    s_pressed = False
+    if cv2.waitKey(25) & 0xFF == ord('s'):
+        s_pressed = True
+
+    if args["press"] == False or s_pressed:
+        if args["gray"] or args["all"]:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imwrite(out_dir + '/gray/' + 'gray_' + "{}".format(i) + '.png', gray)
+
+        if args["sat"] or args["hue"] or args["val"] or args["all"]:
+            frag_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        if args["rgb"] or args["all"]:
+            cv2.imwrite(out_dir + '/rgb/' + "{}".format(i) + '.png', frame)
+
+        if args["red"] or args["all"]:
+            cv2.imwrite(out_dir + '/red/' + 'red_' + "{}".format(i) + '.png', frame[:, :, 0])
+
+        if args["green"] or args["all"]:
+            cv2.imwrite(out_dir + '/green/' + 'green_' + "{}".format(i) + '.png', frame[:, :, 1])
+
+        if args["blue"] or args["all"]:
+            cv2.imwrite(out_dir + '/blue/' + 'blue_' + "{}".format(i) + '.png', frame[:, :, 2])
+
+        if args["hue"] or args["all"]:
+            cv2.imwrite(out_dir + '/hue/' + 'hue_' + "{}".format(i) + '.png', frag_hsv[:, :, 0])
+
+        if args["sat"] or args["all"]:
+            cv2.imwrite(out_dir + '/sat/' + 'sat_' + "{}".format(i) + '.png', frag_hsv[:, :, 1])
+
+        if args["val"] or args["all"]:
+            cv2.imwrite(out_dir + '/val/' + 'val_' + "{}".format(i) + '.png', frag_hsv[:, :, 2])
+
+# Close output window
+cv2.destroyAllWindows()
 
